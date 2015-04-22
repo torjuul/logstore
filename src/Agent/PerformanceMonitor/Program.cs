a8f092ef451d15b1796519e91ed34a84ba750938
Stian Liknes <stianlik@gmail.com>
2015-04-22 16:03:47 +0200

    Playground code for performance monitor

---
+++ src/Agent/PerformanceMonitor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace PerformanceMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            var input = Console.ReadLine();
+            while (input != "exit")
+            {
+                Console.WriteLine("Counter is {0}", counter.NextValue());
+                input = Console.ReadLine();
+            }
+
+        }
+    }
+}
